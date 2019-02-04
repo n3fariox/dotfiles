ed05063cad7615843b24b30f102fcbb991ba9924
K S <n3fariox@gmail.com>
2019-02-04 12:44:05 -0500

    Updates
    * fixed adding .bbashrc to main bbashrc
    * added vscode git tools
    * can choose vs code git tools if vscode is installed
    * checks for root/sudo permissions
    * changes homedirs to be lowercase
    * remove chromium from apt install
    * added variables to control whether fzf or sublime are installed
    * tmux.conf - new panes start in the directory of the current pane

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+INSTALL_FZF=true
+INSTALL_SUBLIME=false
 
 append_line() {
   set -e
@@ -34,6 +36,12 @@ append_line() {
   set +e
 }
 
+vsgit() {
+    git config --global core.editor "code --wait"
+    git config --global merge.tool vscode
+    git config --global diff.tool vscode
+}
+
 echo "################################# Setup links ##################################"
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
@@ -45,24 +53,58 @@ ln -sf ~/dotfiles/bbashrc ~/.bbashrc
     # dconf load /org/mate/terminal/profiles/profile0/ < mtconf
 # fi
 
-if [ -f "~/.bashrc" ]; then
+if [ -f "$HOME/.bashrc" ]; then
     echo "################################# Setup bashrc #################################"
-    append_line ~/.bashrc "source ~/.bbashrc"
-    # echo "source ~/.bbashrc" >> ~/.bashrc
+    append_line 1 "source ~/.bbashrc" ~/.bashrc
+fi
+
+if [ -x "$(command -v xdg-user-dirs-update)" ]; then
+    echo "############################## Updating homedirs... #############################"
+    if [ $(xdg-user-dir DESKTOP) != "$HOME/desktop" ]; then
+        echo "Setting desktop folder..."
+        mv $(xdg-user-dir DESKTOP) $HOME/desktop
+        xdg-user-dirs-update --set DESKTOP $HOME/desktop
+    fi
+    if [ $(xdg-user-dir DOWNLOAD) != "$HOME/downloads" ]; then
+        echo "Setting download folder..."
+        mv $(xdg-user-dir DOWNLOAD) $HOME/downloads
+        xdg-user-dirs-update --set DOWNLOAD $HOME/downloads
+    fi
+    echo "Consolidating media directories..."
+    mkdir -p $HOME/media
+    xdg-user-dirs-update --set DOCUMENTS $HOME/media
+    xdg-user-dirs-update --set MUSIC $HOME/media
+    xdg-user-dirs-update --set PICTURES $HOME/media
+    xdg-user-dirs-update --set VIDEOS $HOME/media
+    echo "Disabling unused directories..."
+    xdg-user-dirs-update --set TEMPLATES $HOME
+    xdg-user-dirs-update --set PUBLICSHARE $HOME
+    echo "Updating directory registry..."
+    xdg-user-dirs-update
+    echo "Attempting to remove default user dirs..."
+    rmdir $HOME/Download 2>/dev/null
+    rmdir $HOME/Documents 2>/dev/null
+    rmdir $HOME/Music 2>/dev/null
+    rmdir $HOME/Pictures  2>/dev/null
+    rmdir $HOME/Videos 2>/dev/null
+    rmdir $HOME/Templates 2>/dev/null
+    rmdir $HOME/Public 2>/dev/null
+
 fi
 
 echo "############################# Installing packages ##############################"
-sudo apt install tmux git meld chromium-browser apt-transport-https
+sudo apt install tmux git meld
 
-if [ ! -d ~/.fzf/ ]; then
+if [ "$INSTALL_FZF" = true ] && [ ! -d ~/.fzf/ ]; then
     echo "################################ Installing fzf ################################"
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
 fi
 
-if [ ! -f /usr/bin/subl ]; then
+if [ "$INSTALL_SUBLIME" = true ] && [ ! -f /usr/bin/subl ]; then
     set -e
     echo "########################### Installing sublime-text ############################"
+    sudo apt install apt-transport-https
     wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
     echo "deb https://download.sublimetext.com/ apt/stable" | sudo tee /etc/apt/sources.list.d/sublime-text.list
     sudo apt update
@@ -76,4 +118,9 @@ if [ ! -f /usr/bin/code ]; then
 else
     echo "########################## Installing VS Code Settings #########################"
     cp vscode-settings.json ~/.config/Code/User/settings.json
+    read -p "Install VS Code gitconfig (y/n)?" choice
+    case "$choice" in
+      y|Y ) vsgit ;;
+      * ) echo "Not updating gitconfig";;
+    esac
 fi
