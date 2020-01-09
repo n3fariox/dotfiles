219071c5c793b99407195c8f918e8fa45740df12
K S <n3fariox@gmail.com>
2020-01-08 22:10:51 -0500

    Updates and bundling
    - include fzf and numix theme since they're great
    - include vscode keybindings to get rid of ctrl-q
    - better checking of root permissions
    - remove user section from gitconfig
    - added some start to versioning tmux config
    - adds some useful aliases

--- setup.sh
+++ setup.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+# First sanity check to see if we're root.
+# If not, have the user enter the sudo password if available
+SCRIPT_UID=$(id -u)
+if [[ "$SCRIPT_UID" != 0 ]]; then
+  { which sudo >/dev/null && sudo test 1 ;} || { echo "non-root" && exit ;}
+  $SUDO="sudo"
+fi
+if [[ ! -z "$SUDO_USER" ]]; then
+  echo "Don't run directly as sudo, it messes with permissions" && exit
+fi
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 INSTALL_FZF=true
 INSTALL_SUBLIME=false
+INSTALL_NUMIX=false
 
 LSB_RELEASE="$(lsb_release -cs)"
-IS_DESKTOP=$(dpkg --get-selections | grep -e 'ubuntu-.*desktop' | wc -l) > 0
+IS_DESKTOP=$(dpkg --get-selections | grep -e 'ubuntu-.*desktop' | wc -l)
+
+if [ "$IS_DESKTOP" -gt 0 ]; then
+    INSTALL_NUMIX=true
+fi
 
 append_line() {
   set -e
@@ -46,10 +63,17 @@ vsgit() {
 }
 
 echo "################################# Setup links ##################################"
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/bbashrc ~/.bbashrc
-ln -sf ~/dotfiles/nanorc ~/.nanorc
+case $(tmux -V | cut -d " " -f 2) in
+*)
+    ln -sf $THIS_DIR/tmux.conf ~/.tmux.conf
+    ;;
+esac
+mkdir -p ~/.tmux/
+# ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+# ln -sf ~/dotfiles/tmux-powerline.conf ~/.tmux/tmux-powerline.conf
+ln -sf $THIS_DIR/gitconfig ~/.gitconfig
+ln -sf $THIS_DIR/bbashrc ~/.bbashrc
+ln -sf $THIS_DIR/nanorc ~/.nanorc
 
 # dconf list /org/mate/terminal/ | grep profiles > /dev/null
 # if [ $? -eq 0 ]; then
@@ -73,6 +97,7 @@ if [ -x "$(command -v xdg-user-dirs-update)" ]; then
         echo "Setting download folder..."
         mv $(xdg-user-dir DOWNLOAD) $HOME/downloads
         xdg-user-dirs-update --set DOWNLOAD $HOME/downloads
+        # TODO: need to set the bluetooth setting if possible
     fi
     echo "Consolidating media directories..."
     mkdir -p $HOME/media
@@ -96,40 +121,54 @@ if [ -x "$(command -v xdg-user-dirs-update)" ]; then
 
 fi
 
-echo "############################# Installing packages ##############################"
-INSTALLS="tmux git"
-if [ "$IS_DESKTOP" -gt 0 ]; then
-    INSTALLS="$INSTALLS meld"
+if [ -x "$(command -v apt-get)" ]; then
+  echo "############################# Installing apt packages ##########################"
+  INSTALLS="tmux git"
+  if [ "$IS_DESKTOP" -gt 0 ]; then
+      INSTALLS="$INSTALLS meld"
+  fi
+  # case $LSB_RELEASE in
+  #     bionic)
+  #         INSTALLS="$INSTALLS powerline"
+  #     ;;
+  # esac
+  $SUDO apt-get install $INSTALLS
 fi
-sudo apt-get install $INSTALLS
 
 if [ "$INSTALL_FZF" = true ] && [ ! -d ~/.fzf/ ]; then
     echo "################################ Installing fzf ################################"
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
+    # git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    # ~/.fzf/install
+    tar xvzf $THIS_DIR/fzf-bin.tar.gz >/dev/null && $THIS_DIR/install-fzf --all
 fi
 
 if [ "$INSTALL_SUBLIME" = true ] && [ ! -f /usr/bin/subl ]; then
     set -e
     echo "########################### Installing sublime-text ############################"
-    sudo apt install apt-transport-https
-    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-    echo "deb https://download.sublimetext.com/ apt/stable" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-    sudo apt update
-    sudo apt install sublime-text
-    ln -s ~/dotfiles/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+    $SUDO apt-get install apt-transport-https
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | $SUDO apt-key add -
+    echo "deb https://download.sublimetext.com/ apt/stable" | $SUDO tee /etc/apt/sources.list.d/sublime-text.list
+    $SUDO apt-get update
+    $SUDO apt-get install sublime-text
+    ln -s $THIS_DIR/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
     git config --global core.editor "subl -n -w"
     set +e
 fi
 
-if [ ! -f /usr/bin/code ]; then
+if [ -x "$(command -v code)"]; then
     echo "########################### VS Code is not installed ###########################"
 else
     echo "########################## Installing VS Code Settings #########################"
-    cp vscode-settings.json ~/.config/Code/User/settings.json
+    cp $THIS_DIR/vscode-settings.json ~/.config/Code/User/settings.json
+    cp $THIS_DIR/vscode-keybindings.json ~/.config/Code/User/keybindings.json
     read -p "Install VS Code gitconfig (y/n)?" choice
     case "$choice" in
       y|Y ) vsgit ;;
       * ) echo "Not updating gitconfig";;
     esac
+fi
+
+if [ "$INSTALL_NUMIX" = true ]; then
+    # Should probably look for a window manager...oh well
+    sudo tar xvf $THIS_DIR/numix-gtk-theme.tar.gz
 fi
