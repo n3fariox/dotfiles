c6a83dae4c8692a21569b77d8f607084a561d357
K S <n3fariox@gmail.com>
2020-04-04 14:09:15 -0400

    New Updates
    - Shellcheck all the bash files
    - bugfixes to setup.sh
    - ensure apt happens before linking (need tmux to detect version)
    - decided just to overwrite any bashrc with my one, no more sourcing an addendum
        - based on ubuntu defaults
    - adds a bash reload func
    - docker buildkit ftw
    - removes unused font file
    - vscode settings update
    - attempts to include most used sublime keybindings so I can function if the extension isn't installed
    - nano tabs to spaces

---
+++ bashrc
@@ -0,0 +1,107 @@
+#!/bin/bash
+# Useful utilities
+## nicdu - ncurses disk usage
+
+# Useful python libs
+## ptpython - fancy interpreter
+
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# Bash control env vars
+HISTCONTROL=ignoreboth
+HISTSIZE=
+HISTFILESIZE=
+shopt -s histappend
+shopt -s checkwinsize
+shopt -s globstar
+
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+################################# BASH PROMPT ##################################
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        # We have color support; assume it's compliant with Ecma-48
+        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+        # a case would tend to support setf rather than setaf.)
+        color_prompt=yes
+    else
+        color_prompt=
+    fi
+    RED="\033[01;31m"
+    GREEN="\033[01;32m"
+    YELLOW="\033[00;33m"
+    BLUE="\033[01;34m"
+    RESET="\033[00m"
+
+    if [ -f "/etc/bash_completion.d/git-prompt" ]; then
+        # shellcheck disable=SC1091
+        . /etc/bash_completion.d/git-prompt
+        # shellcheck disable=SC2016
+        GIT_PS1='$(__git_ps1 "(%s)")'
+    else
+        GIT_PS1=""
+    fi
+
+    if [ "$color_prompt" = yes ]; then
+        PS1="\[$GREEN\]\u@\h\[$RESET\]: \[$YELLOW\]\w\[$RESET\] $GIT_PS1\n\$ "
+    else
+        PS1="\u@\h: \w $GIT_PS1\n\$ "
+    fi
+    unset color_prompt RED GREEN YELLOW BLUE RESET GIT_PS1
+################################## END PROMPT ##################################
+################################### ALIASES ####################################
+    alias py3="python3 -m ptpython"
+    alias py2="python2 -m ptpython"
+    alias grin="grep -rin"
+    alias duh="du -hd1"
+    alias lash="ls -alh"
+    alias ll='ls -alF'
+    alias la='ls -A'
+    alias l='ls -CF'
+    alias shrug='echo "¯\_(ツ)_/¯"'
+    # enable color support of ls and also add handy aliases
+    if [ -x /usr/bin/dircolors ]; then
+        test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+        alias ls='ls --color=auto'
+        alias grep='grep --color=auto'
+        alias fgrep='fgrep --color=auto'
+        alias egrep='egrep --color=auto'
+    fi
+################################# END ALIASES ##################################
+################################## FUNCTIONS ###################################
+    function tmux-update-env() {
+        eval "$(tmux show-env -s)"
+    }
+
+    function reload() {
+        # shellcheck disable=SC1090
+        . ~/.bashrc
+        echo "Reloaded bashrc"
+    }
+################################ END FUNCTIONS #################################
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    # shellcheck disable=SC1091
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    # shellcheck disable=SC1091
+    . /etc/bash_completion
+  fi
+fi
+
+# autostart tmux
+AUTO_TMUX=0
+if [[ -z "$AUTO_TMUX" ]] && which tmux > /dev/null 2>&1; then
+    test -z "$TMUX" && (tmux attach || tmux new-session)
+fi
+
+export DOCKER_BUILDKIT=1
--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
+# Tailored for ubuntu systems
+INSTALL_FZF=true
+INSTALL_SUBLIME=false
+INSTALL_NUMIX=false
 # First sanity check to see if we're root.
 # If not, have the user enter the sudo password if available
 SCRIPT_UID=$(id -u)
 if [[ "$SCRIPT_UID" != 0 ]]; then
   { which sudo >/dev/null && sudo test 1 ;} || { echo "non-root" && exit ;}
-  $SUDO="sudo"
+  SUDO="sudo"
 fi
 if [[ ! -z "$SUDO_USER" ]]; then
   echo "Don't run directly as sudo, it messes with permissions" && exit
 fi
+
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-INSTALL_FZF=true
-INSTALL_SUBLIME=false
-INSTALL_NUMIX=false
-
 LSB_RELEASE="$(lsb_release -cs)"
-IS_DESKTOP=$(dpkg --get-selections | grep -e 'ubuntu-.*desktop' | wc -l)
+IS_DESKTOP=$(dpkg --get-selections | grep -c -e 'ubuntu-.*desktop')
 
 if [ "$IS_DESKTOP" -gt 0 ]; then
     INSTALL_NUMIX=true
@@ -36,15 +36,15 @@ append_line() {
   echo "  - $line"
   if [ -f "$file" ]; then
     if [ $# -lt 4 ]; then
-      lno=$(\grep -nF "$line" "$file" | sed 's/:.*//' | tr '\n' ' ')
+      lno=$(grep -nF "$line" "$file" | sed 's/:.*//' | tr '\n' ' ')
     else
-      lno=$(\grep -nF "$pat" "$file" | sed 's/:.*//' | tr '\n' ' ')
+      lno=$(grep -nF "$pat" "$file" | sed 's/:.*//' | tr '\n' ' ')
     fi
   fi
   if [ -n "$lno" ]; then
     echo "    - Already exists: line #$lno"
   else
-    if [ $update -eq 1 ]; then
+    if [ "$update" -eq 1 ]; then
       [ -f "$file" ] && echo >> "$file"
       echo "$line" >> "$file"
       echo "    + Added"
@@ -62,18 +62,66 @@ vsgit() {
     git config --global diff.tool vscode
 }
 
+if [ -x "$(command -v xdg-user-dirs-update)" ]; then
+    echo "############################## Updating homedirs... ############################"
+    if [ "$(xdg-user-dir DESKTOP)" != "$HOME/desktop" ]; then
+        echo "Setting desktop folder..."
+        mv "$(xdg-user-dir DESKTOP)" "$HOME/desktop"
+        xdg-user-dirs-update --set DESKTOP "$HOME/desktop"
+    fi
+    if [ "$(xdg-user-dir DOWNLOAD)" != "$HOME/downloads" ]; then
+        echo "Setting download folder..."
+        mv "$(xdg-user-dir DOWNLOAD)" "$HOME/downloads"
+        xdg-user-dirs-update --set DOWNLOAD "$HOME/downloads"
+        # TODO: need to set the bluetooth setting if possible
+    fi
+    echo "Consolidating media directories..."
+    mkdir -p "$HOME/media"
+    xdg-user-dirs-update --set DOCUMENTS "$HOME/media"
+    xdg-user-dirs-update --set MUSIC "$HOME/media"
+    xdg-user-dirs-update --set PICTURES "$HOME/media"
+    xdg-user-dirs-update --set VIDEOS "$HOME/media"
+    echo "Disabling unused directories..."
+    xdg-user-dirs-update --set TEMPLATES "$HOME"
+    xdg-user-dirs-update --set PUBLICSHARE "$HOME"
+    echo "Updating directory registry..."
+    xdg-user-dirs-update
+    echo "Attempting to remove default user dirs..."
+    rmdir "$HOME/Download" 2>/dev/null
+    rmdir "$HOME/Documents" 2>/dev/null
+    rmdir "$HOME/Music" 2>/dev/null
+    rmdir "$HOME/Pictures"  2>/dev/null
+    rmdir "$HOME/Videos" 2>/dev/null
+    rmdir "$HOME/Templates" 2>/dev/null
+    rmdir "$HOME/Public" 2>/dev/null
+
+fi
+
+if [ -x "$(command -v apt-get)" ]; then
+  echo "############################# Installing apt packages ##########################"
+  INSTALLS=(tmux git)
+  if [ "$IS_DESKTOP" -gt 0 ]; then
+      # Add desktop specific packages here
+      :
+  fi
+  case $LSB_RELEASE in
+    # Add in release specific packages
+  esac
+  $SUDO apt-get install "${INSTALLS[@]}"
+fi
+
+
 echo "################################# Setup links ##################################"
 case $(tmux -V | cut -d " " -f 2) in
 *)
-    ln -sf $THIS_DIR/tmux.conf ~/.tmux.conf
+    ln -sf "$THIS_DIR/tmux.conf" ~/.tmux.conf
     ;;
 esac
-mkdir -p ~/.tmux/
+# mkdir -p ~/.tmux/
 # ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 # ln -sf ~/dotfiles/tmux-powerline.conf ~/.tmux/tmux-powerline.conf
-ln -sf $THIS_DIR/gitconfig ~/.gitconfig
-ln -sf $THIS_DIR/bbashrc ~/.bbashrc
-ln -sf $THIS_DIR/nanorc ~/.nanorc
+ln -sf "$THIS_DIR/gitconfig" ~/.gitconfig
+ln -sf "$THIS_DIR/nanorc" ~/.nanorc
 
 # dconf list /org/mate/terminal/ | grep profiles > /dev/null
 # if [ $? -eq 0 ]; then
@@ -81,65 +129,18 @@ ln -sf $THIS_DIR/nanorc ~/.nanorc
     # dconf load /org/mate/terminal/profiles/profile0/ < mtconf
 # fi
 
-if [ -f "$HOME/.bashrc" ]; then
-    echo "################################# Setup bashrc #################################"
-    append_line 1 "source ~/.bbashrc" ~/.bashrc
-fi
-
-if [ -x "$(command -v xdg-user-dirs-update)" ]; then
-    echo "############################## Updating homedirs... #############################"
-    if [ $(xdg-user-dir DESKTOP) != "$HOME/desktop" ]; then
-        echo "Setting desktop folder..."
-        mv $(xdg-user-dir DESKTOP) $HOME/desktop
-        xdg-user-dirs-update --set DESKTOP $HOME/desktop
-    fi
-    if [ $(xdg-user-dir DOWNLOAD) != "$HOME/downloads" ]; then
-        echo "Setting download folder..."
-        mv $(xdg-user-dir DOWNLOAD) $HOME/downloads
-        xdg-user-dirs-update --set DOWNLOAD $HOME/downloads
-        # TODO: need to set the bluetooth setting if possible
-    fi
-    echo "Consolidating media directories..."
-    mkdir -p $HOME/media
-    xdg-user-dirs-update --set DOCUMENTS $HOME/media
-    xdg-user-dirs-update --set MUSIC $HOME/media
-    xdg-user-dirs-update --set PICTURES $HOME/media
-    xdg-user-dirs-update --set VIDEOS $HOME/media
-    echo "Disabling unused directories..."
-    xdg-user-dirs-update --set TEMPLATES $HOME
-    xdg-user-dirs-update --set PUBLICSHARE $HOME
-    echo "Updating directory registry..."
-    xdg-user-dirs-update
-    echo "Attempting to remove default user dirs..."
-    rmdir $HOME/Download 2>/dev/null
-    rmdir $HOME/Documents 2>/dev/null
-    rmdir $HOME/Music 2>/dev/null
-    rmdir $HOME/Pictures  2>/dev/null
-    rmdir $HOME/Videos 2>/dev/null
-    rmdir $HOME/Templates 2>/dev/null
-    rmdir $HOME/Public 2>/dev/null
-
-fi
-
-if [ -x "$(command -v apt-get)" ]; then
-  echo "############################# Installing apt packages ##########################"
-  INSTALLS="tmux git"
-  if [ "$IS_DESKTOP" -gt 0 ]; then
-      INSTALLS="$INSTALLS meld"
-  fi
-  # case $LSB_RELEASE in
-  #     bionic)
-  #         INSTALLS="$INSTALLS powerline"
-  #     ;;
-  # esac
-  $SUDO apt-get install $INSTALLS
-fi
+ln -sf "$THIS_DIR/bashrc" ~/.bashrc
+# if [ -f "$HOME/.bashrc" ]; then
+#     echo "################################# Setup bashrc #################################"
+#     ln -sf "$THIS_DIR/bbashrc" ~/.bbashrc
+#     append_line 1 "source ~/.bbashrc" ~/.bashrc
+# fi
 
 if [ "$INSTALL_FZF" = true ] && [ ! -d ~/.fzf/ ]; then
     echo "################################ Installing fzf ################################"
     # git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     # ~/.fzf/install
-    tar xvzf $THIS_DIR/fzf-bin.tar.gz >/dev/null && $THIS_DIR/install-fzf --all
+    tar xvzf "$THIS_DIR/fzf-bin.tar.gz" >/dev/null && "$THIS_DIR/install-fzf" --all
 fi
 
 if [ "$INSTALL_SUBLIME" = true ] && [ ! -f /usr/bin/subl ]; then
@@ -150,25 +151,25 @@ if [ "$INSTALL_SUBLIME" = true ] && [ ! -f /usr/bin/subl ]; then
     echo "deb https://download.sublimetext.com/ apt/stable" | $SUDO tee /etc/apt/sources.list.d/sublime-text.list
     $SUDO apt-get update
     $SUDO apt-get install sublime-text
-    ln -s $THIS_DIR/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+    ln -s "$THIS_DIR/Preferences.sublime-settings" ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
     git config --global core.editor "subl -n -w"
     set +e
 fi
 
-if [ -x "$(command -v code)"]; then
+if [ -x "$(command -v code)" ]; then
     echo "########################### VS Code is not installed ###########################"
 else
     echo "########################## Installing VS Code Settings #########################"
-    cp $THIS_DIR/vscode-settings.json ~/.config/Code/User/settings.json
-    cp $THIS_DIR/vscode-keybindings.json ~/.config/Code/User/keybindings.json
-    read -p "Install VS Code gitconfig (y/n)?" choice
+    cp "$THIS_DIR/vscode-settings.json" ~/.config/Code/User/settings.json
+    cp "$THIS_DIR/vscode-keybindings.json" ~/.config/Code/User/keybindings.json
+    read -r -p "Install VS Code gitconfig (y/n)?" choice
     case "$choice" in
       y|Y ) vsgit ;;
       * ) echo "Not updating gitconfig";;
     esac
 fi
 
-if [ "$INSTALL_NUMIX" = true ]; then
+if [ "$INSTALL_NUMIX" = true ] && [ ! -d /usr/share/themes/Numix/ ]; then
     # Should probably look for a window manager...oh well
-    sudo tar xvf $THIS_DIR/numix-gtk-theme.tar.gz
+   sudo tar xvf "$THIS_DIR/numix-gtk-theme.tar.gz"
 fi
